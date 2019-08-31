c7250ec403a145f0d2a00f017ca975f1de49340b
imooc <827032783@qq.com>
2019-08-31 13:29:28 +0800

    Update GetAccessTokenTask.java

--- src/main/java/cn/scauaie/timer/task/GetAccessTokenTask.java
+++ src/main/java/cn/scauaie/timer/task/GetAccessTokenTask.java
@@ -1,69 +1,69 @@
-package cn.scauaie.timer.task;
-
-import cn.scauaie.manager.WeChatMpManager;
-import cn.scauaie.model.dto.AccessTokenDTO;
-import cn.scauaie.service.CacheService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
-
-import java.time.LocalDateTime;
-import java.util.Optional;
-import java.util.TimerTask;
-
-/**
- * 描述:
- *
- * @author xhsf
- * @email 827032783@qq.com
- * @create 2019-08-30 21:48
- */
-@Component
-public class GetAccessTokenTask extends TimerTask {
-
-    @Autowired
-    private WeChatMpManager weChatMpManager;
-
-    @Autowired
-    private CacheService cacheService;
-
-    /**
-     * 最近一次获取access-token
-     */
-    private LocalDateTime lastGetAccessToken;
-
-    /**
-     * 构造器
-     */
-    public GetAccessTokenTask() {
-        this.init();
-    }
-
-    @Override
-    public void run() {
-
-    }
-
-    public LocalDateTime getLastGetAccessToken() {
-        return lastGetAccessToken;
-    }
-
-    public void setLastGetAccessToken(LocalDateTime lastGetAccessToken) {
-        this.lastGetAccessToken = lastGetAccessToken;
-    }
-
-    /**
-     * 初始化task
-     */
-    private void init() {
-        Optional<AccessTokenDTO> accessToken = weChatMpManager.getAccessToken();
-        if (!accessToken.isPresent()) {
-            this.lastGetAccessToken = LocalDateTime.MIN;
-        } else {
-            this.lastGetAccessToken = LocalDateTime.now();
-            cacheService.set("recruit:wechat:mp:access:token", accessToken.get().getAccess_token());
-        }
-    }
-
-
-
-}
+//package cn.scauaie.timer.task;
+//
+//import cn.scauaie.manager.WeChatMpManager;
+//import cn.scauaie.model.dto.AccessTokenDTO;
+//import cn.scauaie.service.CacheService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.stereotype.Component;
+//
+//import java.time.LocalDateTime;
+//import java.util.Optional;
+//import java.util.TimerTask;
+//
+///**
+// * 描述:
+// *
+// * @author xhsf
+// * @email 827032783@qq.com
+// * @create 2019-08-30 21:48
+// */
+//@Component
+//public class GetAccessTokenTask extends TimerTask {
+//
+//    @Autowired
+//    private WeChatMpManager weChatMpManager;
+//
+//    @Autowired
+//    private CacheService cacheService;
+//
+//    /**
+//     * 最近一次获取access-token
+//     */
+//    private LocalDateTime lastGetAccessToken;
+//
+//    /**
+//     * 构造器
+//     */
+//    public GetAccessTokenTask() {
+//        this.init();
+//    }
+//
+//    @Override
+//    public void run() {
+//
+//    }
+//
+//    public LocalDateTime getLastGetAccessToken() {
+//        return lastGetAccessToken;
+//    }
+//
+//    public void setLastGetAccessToken(LocalDateTime lastGetAccessToken) {
+//        this.lastGetAccessToken = lastGetAccessToken;
+//    }
+//
+//    /**
+//     * 初始化task
+//     */
+//    private void init() {
+//        Optional<AccessTokenDTO> accessToken = weChatMpManager.getAccessToken();
+//        if (!accessToken.isPresent()) {
+//            this.lastGetAccessToken = LocalDateTime.MIN;
+//        } else {
+//            this.lastGetAccessToken = LocalDateTime.now();
+//            cacheService.set("recruit:wechat:mp:access:token", accessToken.get().getAccess_token());
+//        }
+//    }
+//
+//
+//
+//}
