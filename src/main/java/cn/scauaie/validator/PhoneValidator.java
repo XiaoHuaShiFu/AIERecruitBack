c6cb3c536332b3ec21e459c5670592689e653946
imooc <827032783@qq.com>
2019-09-03 13:46:05 +0800

    update

--- src/main/java/cn/scauaie/validator/PhoneValidator.java
+++ src/main/java/cn/scauaie/validator/PhoneValidator.java
@@ -21,7 +21,7 @@ public class PhoneValidator implements ConstraintValidator<Phone, String> {
      * 手机号码匹配模式
      */
     private static final String REGEX_PHONE =
-            "^((13[0-9])|(14[5|7])|(15([0-3]|[5-9]))|(17[013678])|(18[0,5-9])|(19[0-9]))\\d{8}$";
+            "^((13[0-9])|(14[5|7])|(15([0-3]|[5-9]))|(16[0-9])|(17[013678])|(18[0-9])|(19[0-9]))\\d{8}$";
 
     /**
      * 构造静态的匹配模式
